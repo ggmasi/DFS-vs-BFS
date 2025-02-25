bbd9fe7398c159e0021b22edbb0c182feabc0161
Gustavo De Masi <ggmasi123@gmail.com>
2025-02-25 17:06:14 -0300

    Update DFS + comentarios

--- main.c
+++ main.c
@@ -45,48 +45,48 @@ int main(){
 
 }
 
-int DFS(int x, int y, int labirinto[TAM][TAM], int visitado[TAM][TAM], int destino[2], stack *pilha){
-    if(x < 0 || x >= TAM || y < 0 || y >= TAM){
-        return 0;
-    } 
-    if(labirinto[x][y] == 1){
-        return 0;
-    }
+int DFS(int x, int y, int labirinto[TAM][TAM], int visitado[TAM][TAM], int destino[2], stack *pilha) {
+    if (labirinto[x][y] == 1) return 0;
 
-
-    visitado[x][y] = 1;
-    int pos = (x*TAM) + y;
+    int pos = x * TAM + y;
     push(pilha, pos);
-    if(x == destino[0] && y == destino[1]){
-        printf("\nDFS\n");
-        int temp;
-        int solucaoDFS[TAM][TAM] = { 0 };
-        while(!isEmpty(pilha)){
-            temp = pop(pilha);
-            int tempX = temp/TAM;
-            int tempY = temp%TAM;
-            solucaoDFS[tempX][tempY] = 1;
-        }
-        for (int i = 0; i < TAM; i++){
-            for (int j = 0; j < TAM; j++){
-                printf("%d ", solucaoDFS[i][j]);
+    visitado[x][y] = 1;
+
+
+    int dx[] = {1, 0, -1, 0};
+    int dy[] = {0, 1, 0, -1};
+
+    while (!isEmpty(pilha)) {
+        pos = pop(pilha);
+        Posicao atual;
+        atual.x = pos / TAM;
+        atual.y = pos % TAM;
+
+        if (atual.x == destino[0] && atual.y == destino[1]) {
+            printf("\nDFS\n");
+            for (int i = 0; i < TAM; i++) {
+                for (int j = 0; j < TAM; j++) {
+                    printf("%d ", visitado[i][j]);
+                }
+                printf("\n");
             }
-            printf("\n");
-        }
-        return 1;
-    }
-
-    int direcoes[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-    for (int i = 0; i < 4; i++){
-        int novoX = x + direcoes[i][0];
-        int novoY = y + direcoes[i][1];
-        if(DFS(novoX, novoY, labirinto, visitado, destino, pilha)){
             return 1;
         }
+
+        for (int i = 3; i >= 0; i--) {
+            int nx = atual.x + dx[i];
+            int ny = atual.y + dy[i];
+
+            if (nx >= 0 && nx < TAM && ny >= 0 && ny < TAM && labirinto[nx][ny] == 0 && !visitado[nx][ny]) {
+                
+                int new_pos = nx * TAM + ny;
+                push(pilha, new_pos);
+                visitado[nx][ny] = 1;
+                
+            }
+        }
     }
-    
-    pop(pilha);
+
     return 0;
 }
 
